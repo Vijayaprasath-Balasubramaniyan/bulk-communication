c8dd5043c3d8c0d39f212fb3b4a73ab80af1f79a
Vijayaprasath-Balasubramaniyan <vijayprasath109@gmail.com>
2026-01-14 17:51:59 +0530

    fix

--- api/manual_retry.php
+++ api/manual_retry.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../config/Database.php';
-
+require_once __DIR__ . '/../vendor/autoload.php';
 header('Content-Type: application/json');
 
 $pdo = Database::connect();
